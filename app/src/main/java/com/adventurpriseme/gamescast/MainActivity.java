a9a37b8241682462af37233927cb7c0e5ef06b5b
tbremm <juggling.engineer@gmail.com>
2014-09-24 23:14:59 -0400

    Added a games menu button and a submenu of buttons with one displaying a trivial pursuit image. This is a bigtime mockup, and not really how we want to do it.

--- app/src/main/java/com/adventurpriseme/gamescast/MainActivity.java
+++ app/src/main/java/com/adventurpriseme/gamescast/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -12,14 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -28,9 +30,20 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void onGamesMenuSelected (View view) {
+        setContentView(R.layout.activity_gamesmenu);
+    }
+
+    public void on_game_1(View view) {
+        setContentView(R.layout.activity_game1);
+    }
+
+    public void on_game_2(View view) {
+    }
+
+    public void on_game_3(View view) {
     }
 }
